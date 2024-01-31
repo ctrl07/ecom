afbaabb2e97f479f9b8864f74b444fe61e6aaf44
ctrl07 <skgaming7869@gmail.com>
2024-01-31 06:17:50 +0530

    Ecommerce Website in React

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBtBchzUlUrH9p4XZ46g8bMrGKWNZsENdM",
+  authDomain: "react-ecom-e65d3.firebaseapp.com",
+  projectId: "react-ecom-e65d3",
+  storageBucket: "react-ecom-e65d3.appspot.com",
+  messagingSenderId: "1054297559552",
+  appId: "1:1054297559552:web:d0399494c891c91e3b3ee5",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
